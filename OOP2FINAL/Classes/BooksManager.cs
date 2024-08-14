1b11506ff1e53294bfcab37ec73ade2688b5b405
generalpranay <pranayhasmukh.bhalsod@edu.sait.ca>
2024-08-14 02:53:13 -0600

    my changes check please

--- OOP2FINAL/Classes/BooksManager.cs
+++ OOP2FINAL/Classes/BooksManager.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace OOP2FINAL.Classes
 {
-    internal class BooksManager
+    public class BooksManager
     {
+        internal List<Books>books=new List<Books>();
+
         //internal list of Books to iterate through and manage queries
 
         //Load data to list from database
+               //idk this shit !!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
 
         //return list of found books from search
+        internal Books FindBooks(string isbn)
+        {
+            isbn = isbn.ToUpper();
+            foreach (Books book in books) 
+            {
+                if (book.Isbn == isbn)
+                {
+                    return book;
+                }
+            }
+            return new Books();
+        }
 
         //returns individual book from search
 
