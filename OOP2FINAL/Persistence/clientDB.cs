8aa024d274fdcb33ccd66c1c66fe491b205c6932
Dafisto <chrisreid2002@gmail.com>
2024-08-14 10:13:48 -0600

    -persistance and password management

--- OOP2FINAL/Persistence/clientDB.cs
+++ OOP2FINAL/Persistence/clientDB.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
 
 namespace OOP2FINAL.Persistence
 {
-    internal class clientDB
+    public class ClientDB
     {
+        //This is a list of clients that will be generated if a report needs to be created or if new clients are being added or a client is being removed
+        internal List<Client> clients = new List<Client>();
+        //This will be the connection to the database
+        
+        internal void RetreiveClientTable()
+        {
+            clients.Clear();
+            DataBase dataBase = new DataBase();
+
+
+        }
     }
 }
